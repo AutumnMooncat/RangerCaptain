24c808dd5d743e9a61493e29f369dcf6ba523796
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-21 13:47:37 -0300

    Add MissMimic

---
+++ src/main/java/RangerCaptain/cards/MissMimic.java
@@ -0,0 +1,44 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.patches.CantUpgradeFieldPatches;
+import RangerCaptain.powers.GambitPower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class MissMimic extends AbstractEasyCard {
+    public final static String ID = makeID(MissMimic.class.getSimpleName());
+    protected static Animation<TextureRegion> missmimic = loadGifOverlay("Miss_Mimic_idle.gif");
+
+    public MissMimic() {
+        super(ID, 1, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 3;
+        gifOverlay = missmimic;
+        CantUpgradeFieldPatches.CantUpgradeField.preventUpgrades.set(this, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new GambitPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {}
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, RED, WHITE, RED, WHITE, false);
+    }
+}
---
+++ src/main/java/RangerCaptain/powers/GambitPower.java
@@ -0,0 +1,52 @@
+package RangerCaptain.powers;
+
+import RangerCaptain.MainModfile;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
+
+public class GambitPower extends AbstractEasyPower {
+    public static final String POWER_ID = MainModfile.makeID(GambitPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public GambitPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, NAME, PowerType.BUFF, true, owner, amount);
+        priority = 7;
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public float atDamageGive(float damage, DamageInfo.DamageType type) {
+        return type == DamageInfo.DamageType.NORMAL ? damage * 2.0F : damage;
+    }
+
+    @Override
+    public float modifyBlock(float blockAmount) {
+        return blockAmount * 2;
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        flash();
+        if (amount == 1) {
+            addToBot(new VFXAction(new LightningEffect(this.owner.hb.cX, this.owner.hb.cY)));
+            addToBot(new LoseHPAction(this.owner, this.owner, 99999));
+            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        } else {
+            addToBot(new ReducePowerAction(owner, owner, this, 1));
+        }
+
+    }
+}
