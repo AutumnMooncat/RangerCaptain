4f1e5c8e334cac0d3f38d6f2a9fd8f8db585a315
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-22 15:26:54 -0300

    Add Hauntome

---
+++ src/main/java/RangerCaptain/cards/Hauntome.java
@@ -0,0 +1,45 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.patches.CantUpgradeFieldPatches;
+import RangerCaptain.powers.AutoLifePower;
+import RangerCaptain.powers.GhostlyPower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Hauntome extends AbstractEasyCard {
+    public final static String ID = makeID(Hauntome.class.getSimpleName());
+    protected static Animation<TextureRegion> hauntome = loadGifOverlay("Hauntome_idle.gif");
+
+    public Hauntome() {
+        super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 2;
+        gifOverlay = hauntome;
+        CantUpgradeFieldPatches.CantUpgradeField.preventUpgrades.set(this, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new GhostlyPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {}
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, GREEN, WHITE, GREEN, WHITE, false);
+    }
+}
---
+++ src/main/java/RangerCaptain/powers/GhostlyPower.java
@@ -0,0 +1,36 @@
+package RangerCaptain.powers;
+
+import RangerCaptain.MainModfile;
+import RangerCaptain.patches.CantUpgradeFieldPatches;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+public class GhostlyPower extends AbstractEasyPower {
+    public static final String POWER_ID = MainModfile.makeID(GhostlyPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public GhostlyPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public void onExhaust(AbstractCard card) {
+        flash();
+        addToBot(new GainBlockAction(owner, amount));
+        addToBot(new ApplyPowerAction(owner, owner, new VigorPower(owner, amount)));
+    }
+}
