231f64e6f2bcecb9b8ae58ba83f68a3b43c93b60
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 17:43:35 -0300

    Add Burnace

---
+++ src/main/java/RangerCaptain/cards/Burnace.java
@@ -0,0 +1,66 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.actions.BetterSelectCardsInHandAction;
+import RangerCaptain.actions.DamageFollowupAction;
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.powers.ConductivePower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Burnace extends AbstractEasyCard {
+    public final static String ID = makeID(Burnace.class.getSimpleName());
+    protected static Animation<TextureRegion> burnace = loadGifOverlay("Burnace_idle.gif");
+    protected static Animation<TextureRegion> smogmagog = loadGifOverlay("Smogmagog_idle.gif");
+
+    public Burnace() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 4;
+        baseMagicNumber = magicNumber = 3;
+        gifOverlay = burnace;
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new BetterSelectCardsInHandAction(magicNumber, ExhaustAction.TEXT[0], true, true, c -> true, cards -> {
+            for (AbstractCard card : cards) {
+                addToTop(new GainBlockAction(p, block));
+                addToTop(new ExhaustSpecificCardAction(card, p.hand, true));
+            }
+        }));
+    }
+
+    @Override
+    public void upp() {
+        upgradeBlock(2);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = smogmagog;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, DARK_GRAY, WHITE, DARK_GRAY, WHITE, false);
+    }
+}
