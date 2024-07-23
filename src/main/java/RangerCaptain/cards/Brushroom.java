50b99e349f74e68079093dff98279e9ba98e940d
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 13:20:22 -0300

    Add Brushroom

---
+++ src/main/java/RangerCaptain/cards/Brushroom.java
@@ -0,0 +1,67 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.patches.CardCounterPatches;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Brushroom extends AbstractEasyCard {
+    public final static String ID = makeID(Brushroom.class.getSimpleName());
+    protected static Animation<TextureRegion> brushroom = loadGifOverlay("Brushroom_idle.gif");
+    protected static Animation<TextureRegion> fungogh = loadGifOverlay("Fungogh_idle.gif");
+
+    public Brushroom() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 9;
+        baseMagicNumber = magicNumber = 2;
+        gifOverlay = brushroom;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        boolean canUse = super.canUse(p, m);
+        if (!canUse) {
+            return false;
+        } else {
+            if (CardCounterPatches.AttackCountField.attackedThisTurn.get(m) == 0) {
+                canUse = false;
+                cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[1];
+            }
+            return canUse;
+        }
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+        upgradeMagicNumber(1);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = fungogh;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, RED, WHITE, RED, WHITE, false);
+    }
+}
