7e4b8a0baec96b7a769b1cf290fdba025555e28c
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 18:37:04 -0300

    Add DjinnEntonic

---
+++ src/main/java/RangerCaptain/cards/DjinnEntonic.java
@@ -0,0 +1,48 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.patches.CantUpgradeFieldPatches;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class DjinnEntonic extends AbstractEasyCard {
+    public final static String ID = makeID(DjinnEntonic.class.getSimpleName());
+    protected static Animation<TextureRegion> djinn = loadGifOverlay("Djinn_Entonic_idle.gif");
+
+    public DjinnEntonic() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 2;
+        gifOverlay = djinn;
+        CantUpgradeFieldPatches.CantUpgradeField.preventUpgrades.set(this, true);
+        cardsToPreview = new Dazed();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new StrengthPower(p, magicNumber));
+        addToBot(new MakeTempCardInDrawPileAction(new Dazed(), magicNumber, true, true));
+    }
+
+    @Override
+    public void upp() {}
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, darken(PURPLE), WHITE, darken(PURPLE), WHITE, false);
+    }
+}
