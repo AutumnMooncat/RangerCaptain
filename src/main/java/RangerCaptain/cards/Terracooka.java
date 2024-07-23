9f9298cb92520a54b5c491920310aa2065cc88a0
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 08:18:54 -0300

    Add Terracooka

---
+++ src/main/java/RangerCaptain/cards/Terracooka.java
@@ -0,0 +1,50 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.powers.BurnedPower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Terracooka extends AbstractEasyCard {
+    public final static String ID = makeID(Terracooka.class.getSimpleName());
+    protected static Animation<TextureRegion> terracooka = loadGifOverlay("Terracooka_idle.gif");
+    protected static Animation<TextureRegion> coaldron = loadGifOverlay("Coaldron_idle.gif");
+
+    public Terracooka() {
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 5;
+        gifOverlay = terracooka;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToEnemy(m, new BurnedPower(m, p, magicNumber));
+        addToBot(new ExhaustAction(1, false, false, false));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(2);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = coaldron;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, darken(BRONZE), WHITE, darken(BRONZE), WHITE, false);
+    }
+}
