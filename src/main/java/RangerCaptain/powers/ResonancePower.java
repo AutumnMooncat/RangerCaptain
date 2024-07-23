ee815130f1eab36b28895dfeb1e38b9734a4407e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 17:08:58 -0300

    Add VFX to ResonancePower

--- src/main/java/RangerCaptain/powers/ResonancePower.java
+++ src/main/java/RangerCaptain/powers/ResonancePower.java
@@ -1,13 +1,18 @@
 package RangerCaptain.powers;
 
 import RangerCaptain.MainModfile;
+import RangerCaptain.actions.DoAction;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.vfx.combat.ImpactSparkEffect;
 
 public class ResonancePower extends AbstractEasyPower {
     public static final String POWER_ID = MainModfile.makeID(ResonancePower.class.getSimpleName());
@@ -33,7 +38,12 @@ public class ResonancePower extends AbstractEasyPower {
 
     private void checkStacks() {
         if (this.amount >= 3) {
-            addToBot(new DamageAction(this.owner, new DamageInfo(null, TRIGGER_DAMAGE, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_HEAVY, true));// 31
+            addToBot(new DamageAction(this.owner, new DamageInfo(null, TRIGGER_DAMAGE, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_HEAVY, true));
+            addToBot(new DoAction(() -> {
+                for(int i = 0; i < 15; ++i) {
+                    AbstractDungeon.topLevelEffectsQueue.add(new ImpactSparkEffect(owner.hb.cX + MathUtils.random(-20.0F, 20.0F) * Settings.scale, owner.hb.cY + MathUtils.random(-20.0F, 20.0F) * Settings.scale));
+                }
+            }));
             addToBot(new ReducePowerAction(owner, owner, this, 3));
         }
     }
