ecc412bfedc9167d35b5a5793eb96734a5f3fded
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 12:26:02 -0300

    Add Binvader

---
+++ src/main/java/RangerCaptain/cards/Binvader.java
@@ -0,0 +1,64 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Binvader extends AbstractEasyCard {
+    public final static String ID = makeID(Binvader.class.getSimpleName());
+    protected static Animation<TextureRegion> binvader = loadGifOverlay("Binvader_idle.gif");
+    protected static Animation<TextureRegion> binterloper = loadGifOverlay("Binterloper_idle.gif");
+
+    public Binvader() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 6;
+        gifOverlay = binvader;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m != null) {
+            for (int i = 0 ; i < binvasionCount() ; i++) {
+                addToBot(new SFXAction("ATTACK_MAGIC_BEAM_SHORT", 0.5F));
+                addToBot(new VFXAction(new BorderFlashEffect(Color.SKY)));
+                addToBot(new VFXAction(new SmallLaserEffect(p.hb.cX, p.hb.cY, m.hb.cX, m.hb.cY), 0.1F));
+                dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+            }
+        }
+    }
+
+    public int binvasionCount() {
+        return (int) Wiz.getAllCardsInCardGroups(true, true).stream().filter(card -> card instanceof Binvader).count();
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = binterloper;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, GRAY, WHITE, GRAY, WHITE, false);
+    }
+}
