9c287bc434f61324adddb6689a24965ed74c9a96
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 22:46:16 -0300

    Add Pondwalker

---
+++ src/main/java/RangerCaptain/cards/Pondwalker.java
@@ -0,0 +1,52 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.powers.BurnedPower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Pondwalker extends AbstractEasyCard {
+    public final static String ID = makeID(Pondwalker.class.getSimpleName());
+    protected static Animation<TextureRegion> pondwalker = loadGifOverlay("Pondwalker_idle.gif");
+    protected static Animation<TextureRegion> sharktanker = loadGifOverlay("Sharktanker_idle.gif");
+
+    public Pondwalker() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 13;
+        baseMagicNumber = magicNumber = 6;
+        gifOverlay = pondwalker;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
+        Wiz.applyToEnemy(m, new BurnedPower(m, p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+        upgradeMagicNumber(2);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = sharktanker;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, darken(GOLD), WHITE, darken(GOLD), WHITE, false);
+    }
+}
