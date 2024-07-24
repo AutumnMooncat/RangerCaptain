bb00b36b4446794baa0128f188c6b59d0657d78e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-23 22:59:02 -0300

    Add Anathema

---
+++ src/main/java/RangerCaptain/cards/Anathema.java
@@ -0,0 +1,51 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.patches.CantUpgradeFieldPatches;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.StartupCard;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DoubleTapPower;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Anathema extends AbstractEasyCard implements StartupCard {
+    public final static String ID = makeID(Anathema.class.getSimpleName());
+    protected static Animation<TextureRegion> anathema = loadGifOverlay("Anathema_idle.gif");
+
+    public Anathema() {
+        super(ID, 0, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
+        gifOverlay = anathema;
+        CantUpgradeFieldPatches.CantUpgradeField.preventUpgrades.set(this, true);
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new DoubleTapPower(Wiz.adp(), 1));
+    }
+
+    @Override
+    public void upp() {}
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, darken(GREEN), WHITE, darken(GREEN), WHITE, false);
+    }
+
+    @Override
+    public boolean atBattleStartPreDraw() {
+        Wiz.applyToSelf(new DoubleTapPower(Wiz.adp(), 1));
+        return true;
+    }
+}
