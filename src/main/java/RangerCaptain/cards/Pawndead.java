edcf664eef7392b11536e0decff00e7c1e0475f9
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-24 01:04:15 -0300

    Add Pawndead

---
+++ src/main/java/RangerCaptain/cards/Pawndead.java
@@ -0,0 +1,81 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.actions.BetterSelectCardsInHandAction;
+import RangerCaptain.cards.abstracts.AbstractMultiUpgradeCard;
+import RangerCaptain.powers.SummonedPower;
+import RangerCaptain.util.CardArtRoller;
+import RangerCaptain.util.Wiz;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.unique.NightmareAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Pawndead extends AbstractMultiUpgradeCard {
+    public final static String ID = makeID(Pawndead.class.getSimpleName());
+    protected static Animation<TextureRegion> pawndead = loadGifOverlay("Pawndead_idle.gif");
+    protected static Animation<TextureRegion> skelevangelist = loadGifOverlay("Skelevangelist_idle.gif");
+    protected static Animation<TextureRegion> kingrave = loadGifOverlay("Kingrave_idle.gif");
+    protected static Animation<TextureRegion> queenyx = loadGifOverlay("Queenyx_idle.gif");
+
+    public Pawndead() {
+        super(ID, 2, CardType.SKILL, CardRarity.RARE, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 2;
+        gifOverlay = pawndead;
+        baseInfo = info = 0;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new BetterSelectCardsInHandAction(1, NightmareAction.TEXT[0], false, false, c -> true, cards -> {
+            for (AbstractCard card : cards) {
+                Wiz.applyToSelfTop(new SummonedPower(p, magicNumber, card));
+            }
+        }));
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, darken(darken(BLUE)), WHITE, darken(darken(BLUE)), WHITE, false);
+    }
+
+    @Override
+    public void addUpgrades() {
+        addUpgradeData(this::upgrade0);
+        addUpgradeData(this::upgrade1, 0);
+        addUpgradeData(this::upgrade2, 0);
+        setExclusions(1,2);
+    }
+
+    public void upgrade0() {
+        upgradeBaseCost(1);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = skelevangelist;
+    }
+
+    public void upgrade1() {
+        upgradeMagicNumber(1);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[1];
+        initializeTitle();
+        gifOverlay = kingrave;
+        baseInfo = info = 1;
+    }
+
+    public void upgrade2() {
+        upgradeBaseCost(0);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[2];
+        initializeTitle();
+        gifOverlay = queenyx;
+        baseInfo = info = 2;
+    }
+}
---
+++ src/main/java/RangerCaptain/powers/SummonedPower.java
@@ -0,0 +1,41 @@
+package RangerCaptain.powers;
+
+import RangerCaptain.MainModfile;
+import RangerCaptain.util.FormatHelper;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+public class SummonedPower extends AbstractEasyPower {
+    public static final String POWER_ID = MainModfile.makeID(SummonedPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public final AbstractCard card;
+
+    public SummonedPower(AbstractCreature owner, int amount, AbstractCard card) {
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+        this.card = card.makeSameInstanceOf();
+    }
+
+    @Override
+    public void updateDescription() {
+        if (amount == 1) {
+            this.description = DESCRIPTIONS[0] + FormatHelper.prefixWords(CardModifierManager.onRenderTitle(card, card.name), "#y") + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[0] + FormatHelper.prefixWords(Integer.toString(amount), "#b") + DESCRIPTIONS[2] + FormatHelper.prefixWords(CardModifierManager.onRenderTitle(card, card.name), "#y") + DESCRIPTIONS[1];
+        }
+    }
+
+    @Override
+    public void onEnergyRecharge() {
+        flash();
+        addToBot(new MakeTempCardInHandAction(card, amount));
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+}
