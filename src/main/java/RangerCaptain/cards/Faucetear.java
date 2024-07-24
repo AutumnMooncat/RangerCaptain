d9f4fca8c18fe050bd20efa5f9a9571877e05090
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-24 02:08:49 -0300

    Add Faucetear

---
+++ src/main/java/RangerCaptain/cards/Faucetear.java
@@ -0,0 +1,56 @@
+package RangerCaptain.cards;
+
+import RangerCaptain.actions.BetterSelectCardsInHandAction;
+import RangerCaptain.cards.abstracts.AbstractEasyCard;
+import RangerCaptain.util.CardArtRoller;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static RangerCaptain.MainModfile.makeID;
+
+public class Faucetear extends AbstractEasyCard {
+    public final static String ID = makeID(Faucetear.class.getSimpleName());
+    protected static Animation<TextureRegion> faucetear = loadGifOverlay("Faucetear_idle.gif");
+    protected static Animation<TextureRegion> fountess = loadGifOverlay("Fountess_idle.gif");
+
+    public Faucetear() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 2;
+        gifOverlay = faucetear;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new BetterSelectCardsInHandAction(magicNumber, DiscardAction.TEXT[0], true, true, c -> true, cards -> {
+            if (!cards.isEmpty()) {
+                addToTop(new BetterDiscardPileToHandAction(cards.size()));
+            }
+            for (AbstractCard card : cards) {
+                addToTop(new DiscardSpecificCardAction(card, p.hand));
+            }
+        }));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+        name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
+        initializeTitle();
+        gifOverlay = fountess;
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, BLUE, WHITE, BLUE, WHITE, false);
+    }
+}
