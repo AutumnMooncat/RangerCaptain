87af8cbb47ecea588e7b113b9478263353af1690
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-26 15:52:01 -0300

    Add extra logic for Bronze Orbs to not spawn on top of eachother

--- src/main/java/RangerCaptain/patches/LockIntentPatches.java
+++ src/main/java/RangerCaptain/patches/LockIntentPatches.java
@@ -1,11 +1,21 @@
 package RangerCaptain.patches;
 
 import RangerCaptain.powers.TapeJamPower;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import basemod.ReflectionHacks;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.city.BronzeAutomaton;
+import com.megacrit.cardcrawl.monsters.city.BronzeOrb;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+import javassist.expr.NewExpr;
+
+import java.util.Arrays;
 
 public class LockIntentPatches {
     @SpirePatch2(clz = AbstractMonster.class, method = "setMove", paramtypez = {String.class, byte.class, AbstractMonster.Intent.class, int.class, int.class, boolean.class})
@@ -18,6 +28,133 @@ public class LockIntentPatches {
                 return SpireReturn.Return();
             }
             return SpireReturn.Continue();
+        }
+    }
+
+    @SpirePatch2(clz = BronzeAutomaton.class, method = SpirePatch.CLASS)
+    public static class SpawnField {
+        public static SpireField<BronzeOrb[]> leftOrbs = new SpireField<>(() -> new BronzeOrb[5]);
+        public static SpireField<BronzeOrb[]> rightOrbs = new SpireField<>(() -> new BronzeOrb[5]);
+    }
+
+    public static boolean canSpawnLeftOrb(BronzeAutomaton instance) {
+        return Arrays.stream(SpawnField.leftOrbs.get(instance)).anyMatch(orb -> orb == null || orb.isDeadOrEscaped());
+    }
+
+    public static boolean canSpawnRightOrb(BronzeAutomaton instance) {
+        return Arrays.stream(SpawnField.rightOrbs.get(instance)).anyMatch(orb -> orb == null || orb.isDeadOrEscaped());
+    }
+
+    public static AbstractGameAction setLeftOrb(BronzeAutomaton instance, AbstractGameAction action) {
+        if (action instanceof SpawnMonsterAction) {
+            AbstractMonster m = ReflectionHacks.getPrivate(action, SpawnMonsterAction.class, "m");
+            if (m instanceof BronzeOrb) {
+                for (int i = 0 ; i < SpawnField.leftOrbs.get(instance).length; i++) {
+                    if (SpawnField.leftOrbs.get(instance)[i] == null || SpawnField.leftOrbs.get(instance)[i].isDeadOrEscaped()) {
+                        SpawnField.leftOrbs.get(instance)[i] = (BronzeOrb) m;
+                        break;
+                    }
+                }
+            }
+        }
+        return action;
+    }
+
+    public static AbstractGameAction setRightOrb(BronzeAutomaton instance, AbstractGameAction action) {
+        if (action instanceof SpawnMonsterAction) {
+            AbstractMonster m = ReflectionHacks.getPrivate(action, SpawnMonsterAction.class, "m");
+            if (m instanceof BronzeOrb) {
+                for (int i = 0 ; i < SpawnField.rightOrbs.get(instance).length; i++) {
+                    if (SpawnField.rightOrbs.get(instance)[i] == null || SpawnField.rightOrbs.get(instance)[i].isDeadOrEscaped()) {
+                        SpawnField.rightOrbs.get(instance)[i] = (BronzeOrb) m;
+                        break;
+                    }
+                }
+            }
+        }
+        return action;
+    }
+
+    public static final float[] leftXOffsets = new float[]{0, 0, 0, -160, -160};
+    public static final float[] leftYOffsets = new float[]{0, 160, -160, 0, 160};
+    public static final float[] rightXOffsets = new float[]{0, 0, 0, 160, 160};
+    public static final float[] rightYOffsets = new float[]{0, 160, -160, 0, 160};
+
+    public static float getXPos(BronzeAutomaton instance, float xPos, int count) {
+        if (count == 0) {
+            for (int i = 0 ; i < SpawnField.leftOrbs.get(instance).length ; i++) {
+                if (SpawnField.leftOrbs.get(instance)[i] == null || SpawnField.leftOrbs.get(instance)[i].isDeadOrEscaped()) {
+                    return xPos + leftXOffsets[i];
+                }
+            }
+        } else {
+            for (int i = 0 ; i < SpawnField.rightOrbs.get(instance).length ; i++) {
+                if (SpawnField.rightOrbs.get(instance)[i] == null || SpawnField.rightOrbs.get(instance)[i].isDeadOrEscaped()) {
+                    return xPos + rightXOffsets[i];
+                }
+            }
+        }
+        return xPos;
+    }
+
+    public static float getYPos(BronzeAutomaton instance, float yPos, int count) {
+        if (count == 0) {
+            for (int i = 0 ; i < SpawnField.leftOrbs.get(instance).length ; i++) {
+                if (SpawnField.leftOrbs.get(instance)[i] == null || SpawnField.leftOrbs.get(instance)[i].isDeadOrEscaped()) {
+                    return yPos + leftYOffsets[i];
+                }
+            }
+        } else {
+            for (int i = 0 ; i < SpawnField.rightOrbs.get(instance).length ; i++) {
+                if (SpawnField.rightOrbs.get(instance)[i] == null || SpawnField.rightOrbs.get(instance)[i].isDeadOrEscaped()) {
+                    return yPos + rightYOffsets[i];
+                }
+            }
+        }
+        return yPos;
+    }
+
+    @SpirePatch(clz = BronzeAutomaton.class, method = "takeTurn")
+    public static class NegateActionsAndModifyOrbs {
+        @SpireInstrumentPatch
+        public static ExprEditor plz() {
+            return new ExprEditor() {
+                int hits = 0;
+
+                @Override
+                public void edit(NewExpr e) throws CannotCompileException {
+                    if (e.getClassName().equals(BronzeOrb.class.getName())) {
+                        e.replace(
+                                "$1 = "+LockIntentPatches.class.getName()+".getXPos(this, $1, $3);" +
+                                        "$2 = "+LockIntentPatches.class.getName()+".getYPos(this, $2, $3);" +
+                                        "$_ = $proceed($$);"
+                        );
+                    }
+                }
+
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException {
+                    if (hits < 6 && m.getClassName().equals(GameActionManager.class.getName()) && m.getMethodName().equals("addToBottom")) {
+                        switch (hits) {
+                            case 0:
+                            case 1:
+                                m.replace("if ("+LockIntentPatches.class.getName()+".canSpawnLeftOrb(this)) {$proceed($$);}");
+                                break;
+                            case 2:
+                                m.replace("if ("+LockIntentPatches.class.getName()+".canSpawnLeftOrb(this)) {$proceed("+LockIntentPatches.class.getName()+".setLeftOrb(this, $$));}");
+                                break;
+                            case 3:
+                            case 4:
+                                m.replace("if ("+LockIntentPatches.class.getName()+".canSpawnRightOrb(this)) {$proceed($$);}");
+                                break;
+                            case 5:
+                                m.replace("if ("+LockIntentPatches.class.getName()+".canSpawnRightOrb(this)) {$proceed("+LockIntentPatches.class.getName()+".setRightOrb(this, $$));}");
+                                break;
+                        }
+                        hits++;
+                    }
+                }
+            };
         }
     }
 }
