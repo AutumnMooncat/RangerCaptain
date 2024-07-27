d3ea47c3724186f4defd858571cb19a72c7d0a8b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-27 08:48:22 -0300

    Rework SupplyCache

--- src/main/java/RangerCaptain/cards/SupplyCache.java
+++ src/main/java/RangerCaptain/cards/SupplyCache.java
@@ -1,8 +1,10 @@
 package RangerCaptain.cards;
 
+import RangerCaptain.actions.GatherAction;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.common.BetterDiscardPileToHandAction;
+import com.megacrit.cardcrawl.actions.unique.DiscoveryAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ReboundPower;
@@ -16,16 +18,17 @@ public class SupplyCache extends AbstractEasyCard {
     public SupplyCache() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         baseBlock = block = 7;
+        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        addToBot(new BetterDiscardPileToHandAction(1));
+        addToBot(new GatherAction(1, c -> true, false, upgraded));
     }
 
     @Override
     public void upp() {
-        upgradeBlock(3);
+        upgradeBlock(2);
     }
 }
