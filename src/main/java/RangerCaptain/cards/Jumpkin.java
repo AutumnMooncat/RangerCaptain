7920350b7926fd5508246afd02371be5b80e0bcb
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-29 22:38:49 -0300

    Rework Jumpkin

--- src/main/java/RangerCaptain/cards/Jumpkin.java
+++ src/main/java/RangerCaptain/cards/Jumpkin.java
@@ -11,6 +11,8 @@ import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -21,8 +23,9 @@ public class Jumpkin extends AbstractMultiUpgradeCard {
     protected static Animation<TextureRegion> beanstalker = loadGifOverlay("Beanstalker_idle.gif");
 
     public Jumpkin() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
-        baseBlock = block = 5;
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 5;
+        baseSecondMagic = secondMagic = 1;
         gifOverlay = jumpkin;
         baseInfo = info = 0;
     }
@@ -30,11 +33,14 @@ public class Jumpkin extends AbstractMultiUpgradeCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (info == 0) {
-            blck();
-        } else if (info == 1) {
-            Wiz.applyToEnemy(m, new LeechedPower(m, p, magicNumber));
+            Wiz.applyToEnemy(m, new PoisonPower(m, p, magicNumber));
+            Wiz.applyToEnemy(m, new WeakPower(m, secondMagic, false));
+        } else {
+            Wiz.forAllMonstersLiving(mon -> {
+                Wiz.applyToEnemy(mon, new PoisonPower(mon, p, magicNumber));
+                Wiz.applyToEnemy(mon, new WeakPower(mon, secondMagic, false));
+            });
         }
-        addToBot(new CleansePowerAction(p, 1, power -> power.type == AbstractPower.PowerType.DEBUFF));
     }
 
     @Override
@@ -55,18 +61,16 @@ public class Jumpkin extends AbstractMultiUpgradeCard {
     }
 
     public void upgrade0() {
-        baseMagicNumber = magicNumber = 0;
-        upgradeMagicNumber(4);
-        target = CardTarget.ENEMY;
-        baseBlock = block = -1;
+        baseInfo = info = 1;
+        target = CardTarget.ALL_ENEMY;
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         gifOverlay = draculeaf;
-        baseInfo = info = 1;
     }
 
     public void upgrade1() {
-        upgradeBlock(3);
+        upgradeMagicNumber(2);
+        upgradeSecondMagic(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[1];
         initializeTitle();
         gifOverlay = beanstalker;
