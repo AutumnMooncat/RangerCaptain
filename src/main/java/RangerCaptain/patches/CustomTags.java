9442abcdc7f3f2506611ee06f8436006f46745e5
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-08-10 12:15:01 -0300

    Add cardtags to help with fusion mods

--- src/main/java/RangerCaptain/patches/CustomTags.java
+++ src/main/java/RangerCaptain/patches/CustomTags.java
@@ -5,5 +5,15 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 
 public class CustomTags {
     @SpireEnum
-    public static AbstractCard.CardTags RANGER_CAPTAIN_UNUSED;
+    public static AbstractCard.CardTags MAGIC_POISON;
+    @SpireEnum
+    public static AbstractCard.CardTags MAGIC_VULN;
+    @SpireEnum
+    public static AbstractCard.CardTags MAGIC_WEAK;
+    @SpireEnum
+    public static AbstractCard.CardTags MAGIC_BURN;
+    @SpireEnum
+    public static AbstractCard.CardTags MAGIC_CONDUCTIVE;
+    @SpireEnum
+    public static AbstractCard.CardTags SECOND_MAGIC_WEAK;
 }
