229d5f427529c3b629d7c8723a06d594e1c049fc
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-08-10 23:48:45 -0300

    Rework AeroborosMod so it works on 0 and X cost cards

--- src/main/java/RangerCaptain/cardmods/fusion/AeroborosMod.java
+++ src/main/java/RangerCaptain/cardmods/fusion/AeroborosMod.java
@@ -2,8 +2,13 @@ package RangerCaptain.cardmods.fusion;
 
 import RangerCaptain.MainModfile;
 import RangerCaptain.cardmods.fusion.abstracts.AbstractFusionMod;
+import RangerCaptain.util.FormatHelper;
+import RangerCaptain.util.Wiz;
 import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 
 public class AeroborosMod extends AbstractFusionMod {
@@ -16,16 +21,18 @@ public class AeroborosMod extends AbstractFusionMod {
     }
 
     @Override
-    public void onInitialApplication(AbstractCard card) {
-        if (card.cost > 0) {
-            card.cost--;
-            card.costForTurn--;
-        }
+    public String getModDescription(AbstractCard card) {
+        return DESCRIPTION_TEXT[0];
     }
 
     @Override
-    public String getModDescription(AbstractCard card) {
-        return DESCRIPTION_TEXT[0];
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return FormatHelper.insertAfterText(rawDescription, CARD_TEXT[0]);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        Wiz.atb(new GainEnergyAction(1));
     }
 
     @Override
