7200b510e5fe3f73d7f036316c43853351e1b693
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-08-13 12:12:48 -0300

    Rework Dominoth

--- src/main/java/RangerCaptain/cards/Dominoth.java
+++ src/main/java/RangerCaptain/cards/Dominoth.java
@@ -6,13 +6,11 @@ import RangerCaptain.powers.ConductivePower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
 import RangerCaptain.util.Wiz;
-import basemod.BaseMod;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.NoDrawPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -20,53 +18,24 @@ public class Dominoth extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Dominoth.class.getSimpleName());
 
     public Dominoth() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 0;
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         baseMagicNumber = magicNumber = 2;
+        baseSecondMagic = secondMagic = 0;
         setMonsterData(MonsterEnum.DOMINOTH);
         baseInfo = info = 0;
         tags.add(CustomTags.MAGIC_DRAW);
+        tags.add(CustomTags.SECOND_MAGIC_VIGOR);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (info == 1) {
-            Wiz.applyToEnemy(m, new ConductivePower(m, p, secondMagic));
+            Wiz.applyToEnemy(m, new ConductivePower(m, p, (int) (secondMagic + Wiz.adp().hand.group.stream().filter(c -> c != this).count())));
+        }
+        if (info == 0) {
+            Wiz.applyToSelf(new VigorPower(p, (int) (secondMagic + Wiz.adp().hand.group.stream().filter(c -> c != this).count())));
         }
         addToBot(new DrawCardAction(magicNumber));
-        if (info == 0) {
-            dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        }
-    }
-
-    @Override
-    public void applyPowers() {
-        baseDamage = -1;
-        if (info == 0) {
-            baseDamage = (int) Wiz.adp().hand.group.stream().filter(c -> c != this).count();
-            int cardsAvailable = Wiz.adp().drawPile.size() + Wiz.adp().discardPile.size();
-            if (Wiz.adp().hasPower(NoDrawPower.POWER_ID)) {
-                cardsAvailable = 0;
-            }
-            baseDamage += Math.min(magicNumber, cardsAvailable);
-            baseDamage = Math.min(baseDamage, BaseMod.MAX_HAND_SIZE);
-        }
-        super.applyPowers();
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster mo) {
-        baseDamage = -1;
-        if (info == 0) {
-            baseDamage = (int) Wiz.adp().hand.group.stream().filter(c -> c != this).count();
-            int cardsAvailable = Wiz.adp().drawPile.size() + Wiz.adp().discardPile.size();
-            if (Wiz.adp().hasPower(NoDrawPower.POWER_ID)) {
-                cardsAvailable = 0;
-            }
-            baseDamage += Math.min(magicNumber, cardsAvailable);
-            baseDamage = Math.min(baseDamage, BaseMod.MAX_HAND_SIZE);
-        }
-        super.calculateCardDamage(mo);
     }
 
     @Override
@@ -88,30 +57,25 @@ public class Dominoth extends AbstractMultiUpgradeCard {
     }
 
     public void upgrade0() {
-        baseDamage = damage = -1;
-        baseSecondMagic = 0;
-        upgradeMagicNumber(1);
-        upgradeSecondMagic(3);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.WINGLOOM);
         baseInfo = info = 1;
-        type = CardType.SKILL;
-        rollerKey += "Skill";
-        CardArtRoller.computeCard(this);
+        target = CardTarget.ENEMY;
         tags.add(CustomTags.SECOND_MAGIC_CONDUCTIVE);
+        tags.remove(CustomTags.SECOND_MAGIC_VIGOR);
     }
 
     public void upgrade1() {
-        upgradeMagicNumber(1);
-        upgradeSecondMagic(1);
+        //upgradeMagicNumber(1);
+        upgradeSecondMagic(2);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[1];
         initializeTitle();
         setMonsterData(MonsterEnum.MOTHMANIC);
     }
 
     public void upgrade2() {
-        upgradeMagicNumber(1);
+        upgradeSecondMagic(3);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[2];
         initializeTitle();
         setMonsterData(MonsterEnum.TOKUSECT);
