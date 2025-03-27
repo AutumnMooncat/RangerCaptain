3ee5665c931bf7ac2521f1a404edbcec1f52eb39
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-03-27 18:37:01 -0300

    Rework Conductive debuff

--- src/main/java/RangerCaptain/powers/ConductivePower.java
+++ src/main/java/RangerCaptain/powers/ConductivePower.java
@@ -16,12 +16,11 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.vfx.combat.LightningOrbActivateEffect;
 
-public class ConductivePower extends AbstractEasyPower implements HealthBarRenderPower {
+public class ConductivePower extends AbstractEasyPower {
     public static final String POWER_ID = MainModfile.makeID(ConductivePower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    private final Color hpBarColor = new Color(1f, 1f, 0f, 1f);
     private final AbstractCreature source;
 
     public ConductivePower(AbstractCreature owner, AbstractCreature source, int amount) {
@@ -35,21 +34,14 @@ public class ConductivePower extends AbstractEasyPower implements HealthBarRende
     }
 
     @Override
-    public void onUseCard(AbstractCard card, UseCardAction action) {
-        flashWithoutSound();
-        addToBot(new VFXAction(new LightningOrbActivateEffect(owner.hb.cX, owner.hb.cY)));
-        addToBot(new SFXAction("ORB_LIGHTNING_CHANNEL", 0.2f));
-        addToBot(new DamageAction(owner, new DamageInfo(source, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.NONE));
-        addToBot(new ReducePowerAction(owner, owner, this, 1));
-    }
-
-    @Override
-    public int getHealthBarAmount() {
-        return amount;
-    }
-
-    @Override
-    public Color getColor() {
-        return hpBarColor;
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.type == DamageInfo.DamageType.NORMAL && info.owner != null && info.owner != this.owner) {
+            flash();
+            addToTop(new ReducePowerAction(owner, owner, this, 1));
+            addToTop(new DamageAction(owner, new DamageInfo(source, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.NONE));
+            addToTop(new SFXAction("ORB_LIGHTNING_CHANNEL", 0.2f));
+            addToTop(new VFXAction(new LightningOrbActivateEffect(owner.hb.cX, owner.hb.cY)));
+        }
+        return damageAmount;
     }
 }
