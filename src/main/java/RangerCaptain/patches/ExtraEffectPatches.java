ecc393e7b35326dabce9f140fe0bc3f1e4def565
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-03-28 01:10:48 -0300

    Close Encounter now adds the card to the Discovery pile

--- src/main/java/RangerCaptain/patches/ExtraEffectPatches.java
+++ src/main/java/RangerCaptain/patches/ExtraEffectPatches.java
@@ -1,14 +1,18 @@
 package RangerCaptain.patches;
 
-import RangerCaptain.powers.CloseEncounterPower;
 import RangerCaptain.powers.MindMeldPower;
+import RangerCaptain.ui.DiscoveredCardManager;
 import RangerCaptain.util.Wiz;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import javassist.CtBehavior;
 
 public class ExtraEffectPatches {
+    private static boolean yeetCard = false;
+
     @SpirePatch(clz = AbstractCard.class, method = "<class>")
     public static class EffectFields {
         public static SpireField<Integer> mindMeldCount = new SpireField<>(() -> 0);
@@ -38,9 +42,40 @@ public class ExtraEffectPatches {
                     Wiz.applyToSelf(new MindMeldPower(Wiz.adp(), copy));
                 }
             }
-            if (EffectFields.closeEncounter.get(card)) {
+            /*if (EffectFields.closeEncounter.get(card)) {
                 Wiz.applyToSelf(new CloseEncounterPower(Wiz.adp(), card));
+            }*/
+        }
+    }
+
+    @SpirePatch2(clz = UseCardAction.class, method = "update")
+    public static class FlagForDiscovery {
+        @SpireInsertPatch(locator = Locator.class)
+        public static void yeetCheck(UseCardAction __instance, AbstractCard ___targetCard) {
+            if (EffectFields.closeEncounter.get(___targetCard)) {
+                yeetCard = true;
             }
+        }
+
+        public static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher m = new Matcher.MethodCallMatcher(CardGroup.class, "moveToDiscardPile");
+                return LineFinder.findInOrder(ctBehavior, m);
+            }
+        }
+    }
+
+    @SpirePatch2(clz = CardGroup.class, method = "moveToDiscardPile")
+    public static class SendToDiscovery {
+        @SpirePrefixPatch
+        public static SpireReturn<?> yeet(CardGroup __instance, AbstractCard c) {
+            if (yeetCard) {
+                yeetCard = false;
+                DiscoveredCardManager.addCard(c, false);
+                return SpireReturn.Return();
+            }
+            return SpireReturn.Continue();
         }
     }
 }
