7dad4391b2ec4e8b631318ed1fe560b8ca569930
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-04 13:04:47 -0300

    Add SetCostMod, will not work with X or Unplayable

---
+++ src/main/java/RangerCaptain/cardmods/fusion/components/SetCostMod.java
@@ -0,0 +1,37 @@
+package RangerCaptain.cardmods.fusion.components;
+
+import RangerCaptain.MainModfile;
+import RangerCaptain.cardmods.fusion.abstracts.AbstractFusionMod;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class SetCostMod extends AbstractFusionMod {
+    public static final String ID = MainModfile.makeID(SetCostMod.class.getSimpleName());
+    public static final String[] DESCRIPTION_TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+    public static final String[] CARD_TEXT = CardCrawlGame.languagePack.getUIString(ID).EXTRA_TEXT;
+    public int newCost;
+
+    public SetCostMod(int newCost) {
+        super(ID);
+        this.newCost = newCost;
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        if (card.cost >= 0) {
+            card.cost = newCost;
+            card.costForTurn = newCost;
+        }
+    }
+
+    @Override
+    public String getModDescription() {
+        return String.format(DESCRIPTION_TEXT[0], newCost);
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new SetCostMod(newCost);
+    }
+}
