feecbd49ed85046dbdd5f4bc96af4e579d4ca668
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-07 17:55:58 -0300

    Add extra features to action capturing

--- src/main/java/RangerCaptain/patches/ActionCapturePatch.java
+++ src/main/java/RangerCaptain/patches/ActionCapturePatch.java
@@ -9,10 +9,13 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ActionCapturePatch {
+    public static final Consumer<AbstractGameAction> DO_NOTHING = a -> {};
     public static boolean doCapture;
     public static List<AbstractGameAction> capturedActions = new ArrayList<>();
+    public static Consumer<AbstractGameAction> onCapture = DO_NOTHING;
 
     @SpirePatch2(clz = GameActionManager.class, method = "addToBottom")
     @SpirePatch2(clz = GameActionManager.class, method = "addToTop")
@@ -21,14 +24,22 @@ public class ActionCapturePatch {
         public static SpireReturn<Void> yoink(AbstractGameAction action) {
             if (doCapture) {
                 capturedActions.add(action);
+                onCapture.accept(action);
                 return SpireReturn.Return();
             }
             return SpireReturn.Continue();
         }
     }
 
+    public static void clear() {
+        doCapture = false;
+        onCapture = DO_NOTHING;
+        capturedActions.clear();
+    }
+
     public static void releaseToBot() {
         doCapture = false;
+        onCapture = DO_NOTHING;
         for (AbstractGameAction action : capturedActions) {
             AbstractDungeon.actionManager.addToBottom(action);
         }
@@ -37,6 +48,7 @@ public class ActionCapturePatch {
 
     public static void releaseToTop() {
         doCapture = false;
+        onCapture = DO_NOTHING;
         for (int i = capturedActions.size() - 1; i >= 0; i--) {
             AbstractDungeon.actionManager.addToTop(capturedActions.get(i));
         }
