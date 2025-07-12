819c73d6b9486263bdfd2cfc6f47b066947f1251
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-12 13:48:17 -0300

    Move interface into suite

--- src/main/java/RangerCaptain/patches/CardInHandSuite.java
+++ src/main/java/RangerCaptain/patches/CardInHandSuite.java
@@ -1,6 +1,5 @@
 package RangerCaptain.patches;
 
-import RangerCaptain.cards.interfaces.InHandCard;
 import RangerCaptain.util.Wiz;
 import basemod.BaseMod;
 import basemod.interfaces.*;
@@ -231,6 +230,27 @@ public class CardInHandSuite implements
         public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
             Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "powers");
             return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
+    }
+
+    public interface InHandCard {
+        default void onCardUsed(AbstractCard card) {}
+        default void onCardDrawn(AbstractCard card) {}
+        default void onCardExhausted(AbstractCard card) {}
+        default void onDamaged(DamageInfo info) {}
+        default void onStartTurnBlockLoss(int amountLost) {}
+        default void onPowerApplied(AbstractPower power, AbstractCreature target, AbstractCreature source) {}
+        default float atDamageGive(float dmg, DamageInfo.DamageType type, AbstractCreature target, AbstractCard card) {
+            return dmg;
+        }
+        default float atDamageFinalGive(float dmg, DamageInfo.DamageType type, AbstractCreature target, AbstractCard card) {
+            return dmg;
+        }
+        default float onModifyBlock(float blk, AbstractCard card) {
+            return blk;
+        }
+        default float onModifyBlockFinal(float blk, AbstractCard card) {
+            return blk;
         }
     }
 }
