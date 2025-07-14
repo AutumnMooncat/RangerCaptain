42d2041b1bcf58b53f46ba68372cd609e8ec9ead
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-14 16:00:42 -0300

    Rework Nevermort to use Toxin

--- src/main/java/RangerCaptain/cards/Nevermort.java
+++ src/main/java/RangerCaptain/cards/Nevermort.java
@@ -2,16 +2,16 @@ package RangerCaptain.cards;
 
 import RangerCaptain.cardmods.fusion.FusionComponentHelper;
 import RangerCaptain.cardmods.fusion.abstracts.AbstractComponent;
-import RangerCaptain.cardmods.fusion.components.PoisonComponent;
+import RangerCaptain.cardmods.fusion.components.ToxinComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.patches.CustomTags;
+import RangerCaptain.powers.ToxinPower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
 import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.PoisonPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -21,29 +21,29 @@ public class Nevermort extends AbstractEasyCard {
     static {
         new FusionComponentHelper(MonsterEnum.NEVERMORT)
                 .withCost(0)
-                .with(new PoisonComponent(3, AbstractComponent.ComponentTarget.ENEMY_AOE))
+                .with(new ToxinComponent(3, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.APOCROWLYPSE)
                 .withCost(0)
-                .with(new PoisonComponent(5, AbstractComponent.ComponentTarget.ENEMY_AOE))
+                .with(new ToxinComponent(4, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
     }
 
     public Nevermort() {
         super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.ALL_ENEMY);
-        baseMagicNumber = magicNumber = 3;
+        baseMagicNumber = magicNumber = 2;
         setMonsterData(MonsterEnum.NEVERMORT);
-        tags.add(CustomTags.MAGIC_POISON_AOE);
+        tags.add(CustomTags.MAGIC_TOXIN_AOE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new PoisonPower(mon, p, magicNumber)));
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new ToxinPower(mon, magicNumber)));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(2);
+        upgradeMagicNumber(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.APOCROWLYPSE);
