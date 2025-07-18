65651afd95f4a9b8f857cb68992f09cdc2ba9821
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-18 16:44:14 -0300

    Play vfx/sfx immediately in damage vfx mods

--- src/main/java/RangerCaptain/damageMods/effects/LightningOrbVFX.java
+++ src/main/java/RangerCaptain/damageMods/effects/LightningOrbVFX.java
@@ -1,27 +1,21 @@
 package RangerCaptain.damageMods.effects;
 
-import RangerCaptain.MainModfile;
 import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.vfx.combat.LightningOrbActivateEffect;
 
 import static RangerCaptain.MainModfile.makeID;
 
 public class LightningOrbVFX extends AbstractDamageModifier {
     public final static String ID = makeID(LightningOrbVFX.class.getSimpleName());
-    public static final float COOLDOWN = 0.05f;
-
-    public LightningOrbVFX() {
-        MainModfile.sfxCooldowns.put(ID, COOLDOWN);
-    }
 
     @Override
     public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
-        addToTop(new VFXAction(new LightningOrbActivateEffect(target.hb.cX, target.hb.cY)));
-        addToTop(new SFXAction("ORB_LIGHTNING_CHANNEL", 0.2f));
+        AbstractDungeon.effectList.add(new LightningOrbActivateEffect(target.hb.cX, target.hb.cY));
+        CardCrawlGame.sound.play("ORB_LIGHTNING_CHANNEL", 0.2f);
     }
 
     @Override
