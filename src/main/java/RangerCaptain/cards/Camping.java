ba92c69dc5fe352aa819280ccaef312e476c8e48
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-22 10:39:22 -0300

    Camping no longer Upgrades

--- src/main/java/RangerCaptain/cards/Camping.java
+++ src/main/java/RangerCaptain/cards/Camping.java
@@ -3,8 +3,6 @@ package RangerCaptain.cards;
 import RangerCaptain.actions.StashCardsAction;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.util.Wiz;
-import com.megacrit.cardcrawl.actions.common.UpgradeSpecificCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -16,16 +14,13 @@ public class Camping extends AbstractEasyCard {
     public Camping() {
         super(ID, 2, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
         baseBlock = block = 8;
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        addToBot(new StashCardsAction(Wiz.adp().hand, 1, false, false, c -> true, cards -> {
-            for (AbstractCard card : cards) {
-                addToTop(new UpgradeSpecificCardAction(card));
-            }
-        }));
+        addToBot(new StashCardsAction(Wiz.adp().hand, magicNumber, false, false, c -> true, cards -> {}));
     }
 
     @Override
