8947803bcacf96b1d76ad4eab46c94a5f5281c8a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-22 10:37:34 -0300

    Rework Muskrateer

--- src/main/java/RangerCaptain/cards/Muskrateer.java
+++ src/main/java/RangerCaptain/cards/Muskrateer.java
@@ -1,16 +1,16 @@
 package RangerCaptain.cards;
 
+import RangerCaptain.actions.DoAction;
 import RangerCaptain.cardmods.fusion.FusionComponentHelper;
-import RangerCaptain.cardmods.fusion.components.HavocComponent;
+import RangerCaptain.cardmods.fusion.components.ForEachDebuffComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.PlayTopCardAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -19,35 +19,39 @@ public class Muskrateer extends AbstractEasyCard {
 
     static {
         new FusionComponentHelper(MonsterEnum.MUSKRATEER)
-                .withCost(2)
-                .withDamage(4, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)
-                .with(new HavocComponent(1))
+                .withCost(0)
+                .withDamage(6, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)
+                .with(new ForEachDebuffComponent())
                 .register();
         new FusionComponentHelper(MonsterEnum.RATCOUSEL)
-                .withCost(2)
-                .withDamage(6, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)
-                .with(new HavocComponent(1))
+                .withCost(0)
+                .withDamage(10, AbstractGameAction.AttackEffect.BLUNT_HEAVY)
+                .with(new ForEachDebuffComponent())
                 .register();
     }
 
     public Muskrateer() {
-        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 11;
-        baseMagicNumber = magicNumber = 1;
+        super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 3;
         setMonsterData(MonsterEnum.MUSKRATEER);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, upgraded ? AbstractGameAction.AttackEffect.BLUNT_HEAVY : AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
-        for (int i = 0 ; i  < magicNumber ; i++) {
-            addToBot(new PlayTopCardAction(AbstractDungeon.getCurrRoom().monsters.getRandomMonster(null, true, AbstractDungeon.cardRandomRng), true));
-        }
+        addToBot(new DoAction(() -> {
+            if (m != null) {
+                for (AbstractPower power : m.powers) {
+                    if (power.type == AbstractPower.PowerType.DEBUFF) {
+                        dmgTop(m, upgraded ? AbstractGameAction.AttackEffect.BLUNT_HEAVY : AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+                    }
+                }
+            }
+        }));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(4);
+        upgradeDamage(2);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.RATCOUSEL);
