d97e9087f8f6720565e32c5dba1c192439e459b8
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-26 04:35:20 -0300

    Rework Dominoth

--- src/main/java/RangerCaptain/cards/Dominoth.java
+++ src/main/java/RangerCaptain/cards/Dominoth.java
@@ -3,7 +3,6 @@ package RangerCaptain.cards;
 import RangerCaptain.cardmods.fusion.FusionComponentHelper;
 import RangerCaptain.cardmods.fusion.components.ConductiveComponent;
 import RangerCaptain.cardmods.fusion.components.DrawComponent;
-import RangerCaptain.cardmods.fusion.components.VigorComponent;
 import RangerCaptain.cards.abstracts.AbstractMultiUpgradeCard;
 import RangerCaptain.patches.CustomTags;
 import RangerCaptain.powers.ConductivePower;
@@ -14,7 +13,6 @@ import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -22,43 +20,37 @@ public class Dominoth extends AbstractMultiUpgradeCard {
     public final static String ID = makeID(Dominoth.class.getSimpleName());
 
     static {
-        // TODO "for each" modifier
         new FusionComponentHelper(MonsterEnum.DOMINOTH)
                 .withCost(1)
-                .with(new VigorComponent(4), new DrawComponent(1))
+                .with(new ConductiveComponent(3), new DrawComponent(1))
                 .register();
         new FusionComponentHelper(MonsterEnum.WINGLOOM)
                 .withCost(1)
-                .with(new ConductiveComponent(4), new DrawComponent(1))
+                .with(new ConductiveComponent(5), new DrawComponent(1))
                 .register();
         new FusionComponentHelper(MonsterEnum.MOTHMANIC)
                 .withCost(1)
-                .with(new ConductiveComponent(6), new DrawComponent(1))
+                .with(new ConductiveComponent(7), new DrawComponent(1))
                 .register();
         new FusionComponentHelper(MonsterEnum.TOKUSECT)
                 .withCost(1)
-                .with(new VigorComponent(4), new DrawComponent(2))
+                .with(new ConductiveComponent(4), new DrawComponent(2))
                 .register();
     }
 
     public Dominoth() {
-        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 2;
-        baseSecondMagic = secondMagic = 0;
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 1;
+        baseSecondMagic = secondMagic = 4;
         setMonsterData(MonsterEnum.DOMINOTH);
         baseInfo = info = 0;
         tags.add(CustomTags.MAGIC_DRAW);
-        tags.add(CustomTags.SECOND_MAGIC_VIGOR);
+        tags.add(CustomTags.SECOND_MAGIC_CONDUCTIVE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (info == 1) {
-            Wiz.applyToEnemy(m, new ConductivePower(m, p, (int) (secondMagic + Wiz.adp().hand.group.stream().filter(c -> c != this).count())));
-        }
-        if (info == 0) {
-            Wiz.applyToSelf(new VigorPower(p, (int) (secondMagic + Wiz.adp().hand.group.stream().filter(c -> c != this).count())));
-        }
+        Wiz.applyToEnemy(m, new ConductivePower(m, p, secondMagic));
         addToBot(new DrawCardAction(magicNumber));
     }
 
@@ -81,14 +73,10 @@ public class Dominoth extends AbstractMultiUpgradeCard {
     }
 
     public void upgrade0() {
-        // TODO Vigor -> Conductive with no other upgrades?
+        upgradeSecondMagic(2);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.WINGLOOM);
-        baseInfo = info = 1;
-        target = CardTarget.ENEMY;
-        tags.add(CustomTags.SECOND_MAGIC_CONDUCTIVE);
-        tags.remove(CustomTags.SECOND_MAGIC_VIGOR);
     }
 
     public void upgrade1() {
@@ -100,6 +88,7 @@ public class Dominoth extends AbstractMultiUpgradeCard {
 
     public void upgrade2() {
         upgradeMagicNumber(1);
+        upgradeSecondMagic(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[2];
         initializeTitle();
         setMonsterData(MonsterEnum.TOKUSECT);
