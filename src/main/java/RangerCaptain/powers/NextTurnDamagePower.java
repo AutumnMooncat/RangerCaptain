429aef80b26257ff8c8d3ee3de002d67c9ea84cd
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-26 02:08:19 -0300

    Add power icon for next turn damage

--- src/main/java/RangerCaptain/powers/NextTurnDamagePower.java
+++ src/main/java/RangerCaptain/powers/NextTurnDamagePower.java
@@ -10,9 +10,8 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class NextTurnDamagePower extends AbstractPower implements NonStackablePower {
+public class NextTurnDamagePower extends AbstractEasyPower implements NonStackablePower {
     public static final String POWER_ID = MainModfile.makeID(NextTurnDamagePower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
@@ -22,21 +21,20 @@ public class NextTurnDamagePower extends AbstractPower implements NonStackablePo
     public final DamageInfo info;
 
     public NextTurnDamagePower(AbstractCreature owner, AbstractCreature target, DamageInfo info, AbstractGameAction.AttackEffect effect) {
-        this.ID = POWER_ID;
-        this.name = NAME;
-        this.owner = owner;
-        this.amount = info.base;
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, info.base);
         this.target = target;
         this.info = info;
         this.effect = effect;
-        this.type = PowerType.BUFF;
-        this.loadRegion("infiniteBlades");
         updateDescription();
     }
 
     @Override
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0] + info.base + DESCRIPTIONS[1] + FormatHelper.prefixWords(target.name, "#y") + DESCRIPTIONS[2];
+        if (info == null) {
+            this.description = "???";
+        } else {
+            this.description = DESCRIPTIONS[0] + info.base + DESCRIPTIONS[1] + FormatHelper.prefixWords(target.name, "#y") + DESCRIPTIONS[2];
+        }
     }
 
     @Override
