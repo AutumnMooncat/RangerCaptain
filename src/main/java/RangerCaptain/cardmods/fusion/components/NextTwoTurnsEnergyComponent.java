2a97caefd4bc3791d82ba8a969e5446aa8372401
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-31 01:59:19 -0300

    Rework Jumpkin

---
+++ src/main/java/RangerCaptain/cardmods/fusion/components/NextTwoTurnsEnergyComponent.java
@@ -0,0 +1,66 @@
+package RangerCaptain.cardmods.fusion.components;
+
+import RangerCaptain.MainModfile;
+import RangerCaptain.cardmods.fusion.abstracts.AbstractComponent;
+import RangerCaptain.powers.APBoostPower;
+import RangerCaptain.powers.NextTurnPowerLaterPower;
+import RangerCaptain.util.Wiz;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.List;
+
+public class NextTwoTurnsEnergyComponent extends AbstractComponent {
+    public static final String ID = MainModfile.makeID(NextTwoTurnsEnergyComponent.class.getSimpleName());
+    public static final String[] DESCRIPTION_TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+    public static final String[] CARD_TEXT = CardCrawlGame.languagePack.getUIString(ID).EXTRA_TEXT;
+
+    public NextTwoTurnsEnergyComponent(int base) {
+        super(ID, base, ComponentType.DO, ComponentTarget.NONE, DynVar.MAGIC);
+    }
+
+    @Override
+    public void updatePrio() {
+        priority = DO_PRIO;
+    }
+
+    @Override
+    public String componentDescription() {
+        return DESCRIPTION_TEXT[0];
+    }
+
+    @Override
+    public String rawCardText(List<AbstractComponent> captured) {
+        if (dynvar == DynVar.FLAT) {
+            if (baseAmount >= 1 && baseAmount <= 3) {
+                return CARD_TEXT[baseAmount];
+            }
+            return String.format(CARD_TEXT[4], baseAmount);
+        }
+        return String.format(CARD_TEXT[0], dynKey());
+    }
+
+    @Override
+    public String rawCapturedText() {
+        int offset = 5;
+        if (dynvar == DynVar.FLAT) {
+            if (baseAmount >= 1 && baseAmount <= 3) {
+                return CARD_TEXT[baseAmount + offset];
+            }
+            return String.format(CARD_TEXT[4 + offset], baseAmount);
+        }
+        return String.format(CARD_TEXT[offset], dynKey());
+    }
+
+    @Override
+    public void onTrigger(ComponentAmountProvider provider, AbstractPlayer p, AbstractMonster m, List<AbstractComponent> captured) {
+        Wiz.applyToSelf(new APBoostPower(p, provider.getAmount(this)));
+        Wiz.applyToSelf(new NextTurnPowerLaterPower(p, new APBoostPower(p, provider.getAmount(this))));
+    }
+
+    @Override
+    public AbstractComponent makeCopy() {
+        return new NextTwoTurnsEnergyComponent(baseAmount);
+    }
+}
--- src/main/java/RangerCaptain/cards/Jumpkin.java
+++ src/main/java/RangerCaptain/cards/Jumpkin.java
@@ -1,11 +1,12 @@
 package RangerCaptain.cards;
 
 import RangerCaptain.cardmods.fusion.FusionComponentHelper;
-import RangerCaptain.cardmods.fusion.abstracts.AbstractComponent;
+import RangerCaptain.cardmods.fusion.components.NextTwoTurnsEnergyComponent;
 import RangerCaptain.cardmods.fusion.components.ToxinComponent;
-import RangerCaptain.cardmods.fusion.components.WeakComponent;
 import RangerCaptain.cards.abstracts.AbstractMultiUpgradeCard;
 import RangerCaptain.patches.CustomTags;
+import RangerCaptain.powers.APBoostPower;
+import RangerCaptain.powers.NextTurnPowerLaterPower;
 import RangerCaptain.powers.ToxinPower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
@@ -13,7 +14,6 @@ import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -22,43 +22,39 @@ public class Jumpkin extends AbstractMultiUpgradeCard {
 
     static {
         new FusionComponentHelper(MonsterEnum.JUMPKIN)
-                .withCost(1)
+                .withCost(2)
                 .with(new ToxinComponent(3))
-                .with(new WeakComponent(1))
+                .with(new NextTwoTurnsEnergyComponent(1))
+                .withExhaust()
                 .register();
         new FusionComponentHelper(MonsterEnum.DRACULEAF)
                 .withCost(1)
-                .with(new ToxinComponent(3, AbstractComponent.ComponentTarget.ENEMY_AOE))
-                .with(new WeakComponent(1, AbstractComponent.ComponentTarget.ENEMY_AOE))
+                .with(new ToxinComponent(5))
+                .with(new NextTwoTurnsEnergyComponent(1))
+                .withExhaust()
                 .register();
         new FusionComponentHelper(MonsterEnum.BEANSTALKER)
-                .withCost(1)
+                .withCost(2)
                 .with(new ToxinComponent(4))
-                .with(new WeakComponent(2))
+                .with(new NextTwoTurnsEnergyComponent(1))
+                .withExhaust()
                 .register();
     }
 
     public Jumpkin() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 4;
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY); // 2 cost?
+        baseMagicNumber = magicNumber = 5;
         baseSecondMagic = secondMagic = 1;
         setMonsterData(MonsterEnum.JUMPKIN);
-        baseInfo = info = 0;
         tags.add(CustomTags.MAGIC_TOXIN);
-        tags.add(CustomTags.SECOND_MAGIC_WEAK);
+        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (info == 0) {
-            Wiz.applyToEnemy(m, new ToxinPower(m, magicNumber));
-            Wiz.applyToEnemy(m, new WeakPower(m, secondMagic, false));
-        } else {
-            Wiz.forAllMonstersLiving(mon -> {
-                Wiz.applyToEnemy(mon, new ToxinPower(mon, magicNumber));
-                Wiz.applyToEnemy(mon, new WeakPower(mon, secondMagic, false));
-            });
-        }
+        Wiz.applyToEnemy(m, new ToxinPower(m, magicNumber));
+        Wiz.applyToSelf(new APBoostPower(p, secondMagic));
+        Wiz.applyToSelf(new NextTurnPowerLaterPower(p, new APBoostPower(p, secondMagic)));
     }
 
     @Override
@@ -79,18 +75,14 @@ public class Jumpkin extends AbstractMultiUpgradeCard {
     }
 
     public void upgrade0() {
-        baseInfo = info = 1;
-        target = CardTarget.ALL_ENEMY;
+        upgradeBaseCost(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.DRACULEAF);
-        tags.add(CustomTags.MAGIC_TOXIN_AOE);
-        tags.remove(CustomTags.MAGIC_TOXIN);
     }
 
     public void upgrade1() {
-        upgradeMagicNumber(1);
-        upgradeSecondMagic(1);
+        upgradeMagicNumber(2);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[1];
         initializeTitle();
         setMonsterData(MonsterEnum.BEANSTALKER);
