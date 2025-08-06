46657e6c33301f6e0b4ec54ffc8a56a0bd36d415
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-06 17:07:20 -0300

    Rework Springheel

--- src/main/java/RangerCaptain/cards/Springheel.java
+++ src/main/java/RangerCaptain/cards/Springheel.java
@@ -1,21 +1,19 @@
 package RangerCaptain.cards;
 
 import RangerCaptain.cardmods.fusion.FusionComponentHelper;
-import RangerCaptain.cardmods.fusion.components.DrawComponent;
-import RangerCaptain.cardmods.fusion.components.VulnerableComponent;
-import RangerCaptain.cardmods.fusion.components.WeakComponent;
+import RangerCaptain.cardmods.fusion.abstracts.AbstractComponent;
+import RangerCaptain.cardmods.fusion.components.NextTurnDamageComponent;
 import RangerCaptain.cards.abstracts.AbstractMultiUpgradeCard;
-import RangerCaptain.patches.CustomTags;
+import RangerCaptain.powers.NextTurnDamagePower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
 import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -25,48 +23,56 @@ public class Springheel extends AbstractMultiUpgradeCard {
     static {
         new FusionComponentHelper(MonsterEnum.SPRINGHEEL)
                 .withCost(1)
-                .withDamage(5, AbstractGameAction.AttackEffect.BLUNT_LIGHT)
-                .with(new DrawComponent(2))
+                .withBlock(5)
+                .with(new NextTurnDamageComponent(3, AbstractGameAction.AttackEffect.BLUNT_LIGHT, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.HOPSKIN)
                 .withCost(1)
-                .withDamage(6, AbstractGameAction.AttackEffect.BLUNT_LIGHT)
-                .with(new DrawComponent(2), new VulnerableComponent(1))
+                .withBlock(5)
+                .with(new NextTurnDamageComponent(5, AbstractGameAction.AttackEffect.SLASH_DIAGONAL, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.RIPTERRA)
                 .withCost(1)
-                .withDamage(7, AbstractGameAction.AttackEffect.BLUNT_LIGHT)
-                .with(new DrawComponent(2), new VulnerableComponent(2))
+                .withBlock(5)
+                .with(new NextTurnDamageComponent(8, AbstractGameAction.AttackEffect.SLASH_HEAVY, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.SNOOPIN)
-                .withCost(1)
-                .withDamage(6, AbstractGameAction.AttackEffect.BLUNT_LIGHT)
-                .with(new DrawComponent(2), new WeakComponent(1))
+                .withCost(0)
+                .withBlock(6)
+                .with(new NextTurnDamageComponent(4, AbstractGameAction.AttackEffect.BLUNT_LIGHT, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.SCAMPIRE)
-                .withCost(1)
-                .withDamage(7, AbstractGameAction.AttackEffect.BLUNT_LIGHT)
-                .with(new DrawComponent(2), new WeakComponent(2))
+                .withCost(0)
+                .withBlock(8)
+                .with(new NextTurnDamageComponent(5, AbstractGameAction.AttackEffect.BLUNT_HEAVY, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
     }
 
     public Springheel() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 7;
-        baseMagicNumber = magicNumber = 2;
-        baseSecondMagic = secondMagic = 0;
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        baseBlock = block = 6;
+        baseDamage = damage = 4;
+        isMultiDamage = true;
         setMonsterData(MonsterEnum.SPRINGHEEL);
         baseInfo = info = 0;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        addToBot(new DrawCardAction(magicNumber));
+        blck();
+        AbstractGameAction.AttackEffect fx = AbstractGameAction.AttackEffect.BLUNT_LIGHT;
         if (info == 1) {
-            Wiz.applyToEnemy(m, new VulnerablePower(m, secondMagic, false));
+            fx = AbstractGameAction.AttackEffect.SLASH_DIAGONAL;
         } else if (info == 2) {
-            Wiz.applyToEnemy(m, new WeakPower(m, secondMagic, false));
+            fx = AbstractGameAction.AttackEffect.SLASH_HEAVY;
+        } else if (info == 3) {
+            fx = AbstractGameAction.AttackEffect.BLUNT_HEAVY;
+        }
+        for (int i = 0; i < AbstractDungeon.getMonsters().monsters.size(); i++) {
+            AbstractMonster mon = AbstractDungeon.getMonsters().monsters.get(i);
+            if (!mon.isDeadOrEscaped()) {
+                Wiz.applyToSelf(new NextTurnDamagePower(p, mon, new DamageInfo(p, multiDamage[i], damageTypeForTurn), fx));
+            }
         }
     }
 
@@ -90,38 +96,35 @@ public class Springheel extends AbstractMultiUpgradeCard {
     }
 
     public void upgrade0() {
-        upgradeDamage(2);
-        upgradeSecondMagic(1);
+        upgradeDamage(3);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.HOPSKIN);
         baseInfo = info = 1;
-        tags.add(CustomTags.MAGIC_VULN);
     }
 
     public void upgrade1() {
-        upgradeDamage(2);
-        upgradeSecondMagic(1);
+        upgradeDamage(3);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[1];
         initializeTitle();
         setMonsterData(MonsterEnum.RIPTERRA);
+        baseInfo = info = 2;
     }
 
     public void upgrade2() {
-        upgradeDamage(2);
-        upgradeSecondMagic(1);
+        upgradeBlock(2);
+        upgradeDamage(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[2];
         initializeTitle();
         setMonsterData(MonsterEnum.SNOOPIN);
-        baseInfo = info = 2;
-        tags.add(CustomTags.MAGIC_WEAK);
     }
 
     public void upgrade3() {
-        upgradeDamage(2);
-        upgradeSecondMagic(1);
+        upgradeBlock(2);
+        upgradeDamage(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[3];
         initializeTitle();
         setMonsterData(MonsterEnum.SCAMPIRE);
+        baseInfo = info = 3;
     }
 }
