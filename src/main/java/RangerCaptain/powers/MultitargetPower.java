f1543f3589fd49779119b701f82af2429eb3a0d7
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-10 18:01:02 -0300

    Rework MultitargetPower

--- src/main/java/RangerCaptain/powers/MultitargetPower.java
+++ src/main/java/RangerCaptain/powers/MultitargetPower.java
@@ -5,8 +5,11 @@ import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 
 public class MultitargetPower extends AbstractEasyPower {
@@ -30,13 +33,21 @@ public class MultitargetPower extends AbstractEasyPower {
 
     @Override
     public void onUseCard(AbstractCard card, UseCardAction action) {
-        if (card.target == AbstractCard.CardTarget.ENEMY || card.target == AbstractCard.CardTarget.SELF_AND_ENEMY) {
+        if (!card.purgeOnUse && card.target == AbstractCard.CardTarget.ENEMY || card.target == AbstractCard.CardTarget.SELF_AND_ENEMY) {
             if (card.type == AbstractCard.CardType.ATTACK && action.target != null) {
                 flash();
                 Wiz.forAllMonstersLiving(mon -> {
                     if (mon != action.target) {
-                        card.calculateCardDamage(mon);
-                        card.use(Wiz.adp(), mon);
+                        AbstractCard tmp = card.makeSameInstanceOf();
+                        AbstractDungeon.player.limbo.addToBottom(tmp);
+                        tmp.current_x = card.current_x;
+                        tmp.current_y = card.current_y;
+                        tmp.target_x = Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+                        tmp.target_y = Settings.HEIGHT / 2.0F;
+                        tmp.calculateCardDamage(mon);
+
+                        tmp.purgeOnUse = true;
+                        AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, mon, card.energyOnUse, true, true), true);
                     }
                 });
                 addToTop(new ReducePowerAction(owner, owner, this, 1));
