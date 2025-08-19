a8a82437a2b3f3444bade3525e84dbde0f5354bc
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-19 18:05:56 -0300

    Rework Khepri to remove CloseEncounter

--- src/main/java/RangerCaptain/cards/Khepri.java
+++ src/main/java/RangerCaptain/cards/Khepri.java
@@ -1,15 +1,20 @@
 package RangerCaptain.cards;
 
+import RangerCaptain.actions.BetterSelectCardsInHandAction;
 import RangerCaptain.cardfusion.FusionComponentHelper;
+import RangerCaptain.cardfusion.abstracts.AbstractComponent;
+import RangerCaptain.cardfusion.components.DamageComponent;
 import RangerCaptain.cardfusion.components.ExhaustCardsComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.patches.CantUpgradeFieldPatches;
 import RangerCaptain.patches.CustomTags;
-import RangerCaptain.patches.ExtraEffectPatches;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
+import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -22,27 +27,29 @@ public class Khepri extends AbstractEasyCard {
     static {
         new FusionComponentHelper(MonsterEnum.KHEPRI)
                 .withCost(0)
-                .withDamage(5, AbstractGameAction.AttackEffect.FIRE)
-                .with(new ExhaustCardsComponent(0.92f))
-                .withCloseEncounter()
+                .with(new ExhaustCardsComponent(2.5f, ExhaustCardsComponent.TargetPile.HAND, true, false))
+                .withFlags(new DamageComponent(2, AbstractGameAction.AttackEffect.FIRE), AbstractComponent.Flag.EXHAUST_FOLLOWUP)
                 .register();
     }
 
     public Khepri() {
         super(ID, 0, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 6;
-        baseMagicNumber = magicNumber = 1;
+        baseDamage = damage = 3;
+        baseMagicNumber = magicNumber = 3;
         setMonsterData(MonsterEnum.KHEPRI);
         CantUpgradeFieldPatches.CantUpgradeField.preventUpgrades.set(this, true);
-        tags.add(CustomTags.CLOSE_ENCOUNTER);
         tags.add(CustomTags.MAGIC_EXHAUST);
-        ExtraEffectPatches.EffectFields.closeEncounter.set(this, true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        addToBot(new ExhaustAction(magicNumber, false, false, false));
+        addToBot(new BetterSelectCardsInHandAction(magicNumber, ExhaustAction.TEXT[0], true, true, c -> true, cards -> {
+            for (int i = 0; i < cards.size(); i++) {
+                AbstractCard card = cards.get(i);
+                dmgTop(m, AbstractGameAction.AttackEffect.FIRE);
+                addToTop(new ExhaustSpecificCardAction(card, Wiz.adp().hand));
+            }
+        }));
     }
 
     @Override
