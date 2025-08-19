0b4568755f02384fcd39da4e0bf893e040af9137
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-19 16:45:40 -0300

    Rework Minortom

--- src/main/java/RangerCaptain/cards/Minortom.java
+++ src/main/java/RangerCaptain/cards/Minortom.java
@@ -2,13 +2,13 @@ package RangerCaptain.cards;
 
 import RangerCaptain.cardfusion.FusionComponentHelper;
 import RangerCaptain.cardfusion.components.DrawComponent;
-import RangerCaptain.cardfusion.components.StashNextCardComponent;
+import RangerCaptain.cardfusion.components.MakeCardsComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
-import RangerCaptain.powers.CloseEncounterPower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
-import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -20,26 +20,28 @@ public class Minortom extends AbstractEasyCard {
 
     static {
         new FusionComponentHelper(MonsterEnum.MINORTOM)
-                .withCost(1)
-                .with(new DrawComponent(1.5f), new StashNextCardComponent(1))
+                .withCost(0)
+                .with(new DrawComponent(1.91f))
+                .with(new MakeCardsComponent(0.91f, new VoidCard(), false, MakeCardsComponent.Location.DISCARD))
                 .register();
         new FusionComponentHelper(MonsterEnum.MAJORTOM)
-                .withCost(1)
-                .with(new DrawComponent(2.5f), new StashNextCardComponent(1))
+                .withCost(0)
+                .with(new DrawComponent(2.91f))
+                .with(new MakeCardsComponent(0.91f, new VoidCard(), false, MakeCardsComponent.Location.DISCARD))
                 .register();
     }
 
     public Minortom() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
         baseMagicNumber = magicNumber = 2;
-        baseSecondMagic = secondMagic = 1;
         setMonsterData(MonsterEnum.MINORTOM);
+        cardsToPreview = new VoidCard();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DrawCardAction(magicNumber));
-        Wiz.applyToSelf(new CloseEncounterPower(p, secondMagic));
+        addToBot(new MakeTempCardInDiscardAction(new VoidCard(), 1));
     }
 
     @Override
