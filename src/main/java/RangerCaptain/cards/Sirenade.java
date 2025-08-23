87fa01aff09a081611c3749380e066440e9f2dcd
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-23 20:15:17 -0300

    Rework Sirenade

--- src/main/java/RangerCaptain/cards/Sirenade.java
+++ src/main/java/RangerCaptain/cards/Sirenade.java
@@ -1,11 +1,11 @@
 package RangerCaptain.cards;
 
 import RangerCaptain.cardfusion.FusionComponentHelper;
-import RangerCaptain.cardfusion.components.MultitargetComponent;
+import RangerCaptain.cardfusion.abstracts.AbstractComponent;
+import RangerCaptain.cardfusion.components.WeakComponent;
 import RangerCaptain.cardfusion.components.vfx.PiercingWailVFXComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.patches.CustomTags;
-import RangerCaptain.powers.MultitargetPower;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
 import RangerCaptain.util.Wiz;
@@ -16,6 +16,7 @@ import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
 import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
 
 import static RangerCaptain.MainModfile.makeID;
@@ -27,20 +28,20 @@ public class Sirenade extends AbstractEasyCard {
         new FusionComponentHelper(MonsterEnum.SIRENADE)
                 .withCost(1)
                 .with(new PiercingWailVFXComponent())
-                .withDamageAOE(5, AbstractGameAction.AttackEffect.BLUNT_HEAVY)
-                .with(new MultitargetComponent(1))
+                .withDamageAOE(4, AbstractGameAction.AttackEffect.BLUNT_HEAVY)
+                .with(new WeakComponent(0.91f, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
         new FusionComponentHelper(MonsterEnum.DECIBELLE)
                 .withCost(1)
                 .with(new PiercingWailVFXComponent())
-                .withDamageAOE(7.5f, AbstractGameAction.AttackEffect.BLUNT_HEAVY)
-                .with(new MultitargetComponent(1))
+                .withDamageAOE(5.5f, AbstractGameAction.AttackEffect.BLUNT_HEAVY)
+                .with(new WeakComponent(1.91f, AbstractComponent.ComponentTarget.ENEMY_AOE))
                 .register();
     }
 
     public Sirenade() {
         super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
-        baseDamage = damage = 7;
+        baseDamage = damage = 5;
         baseMagicNumber = magicNumber = 1;
         isMultiDamage = true;
         setMonsterData(MonsterEnum.SIRENADE);
@@ -52,12 +53,13 @@ public class Sirenade extends AbstractEasyCard {
         addToBot(new SFXAction("ATTACK_PIERCING_WAIL"));
         addToBot(new VFXAction(p, new ShockWaveEffect(p.hb.cX, p.hb.cY, Settings.PURPLE_COLOR, ShockWaveEffect.ShockWaveType.CHAOTIC), 0.3F));
         allDmg(AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        Wiz.applyToSelf(new MultitargetPower(p, magicNumber));
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new WeakPower(mon, magicNumber, false)));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.DECIBELLE);
