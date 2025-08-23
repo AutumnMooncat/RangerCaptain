5b7f395445d95d589dd8e83bea7f658c4331f8c0
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-23 14:53:51 -0300

    Rework Sanzatime

--- src/main/java/RangerCaptain/cards/Sanzatime.java
+++ src/main/java/RangerCaptain/cards/Sanzatime.java
@@ -3,15 +3,12 @@ package RangerCaptain.cards;
 import RangerCaptain.actions.ResolveNextTurnEffectsAction;
 import RangerCaptain.cardfusion.FusionComponentHelper;
 import RangerCaptain.cardfusion.components.ResolveNextTurnEffectsComponent;
-import RangerCaptain.cardfusion.components.WeakComponent;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.util.CardArtRoller;
 import RangerCaptain.util.MonsterEnum;
-import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static RangerCaptain.MainModfile.makeID;
 
@@ -20,31 +17,32 @@ public class Sanzatime extends AbstractEasyCard {
 
     static {
         new FusionComponentHelper(MonsterEnum.SANZATIME)
-                .withCost(1)
-                .with(new WeakComponent(1.5f), new ResolveNextTurnEffectsComponent())
+                .withCost(2)
+                .withBlock(8.5f)
+                .with(new ResolveNextTurnEffectsComponent())
                 .register();
         new FusionComponentHelper(MonsterEnum.FORTIWINX)
-                .withCost(0)
-                .with(new WeakComponent(1.5f), new ResolveNextTurnEffectsComponent())
+                .withCost(2)
+                .withBlock(11)
+                .with(new ResolveNextTurnEffectsComponent())
                 .register();
     }
 
     public Sanzatime() {
-        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 2;
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 12;
         setMonsterData(MonsterEnum.SANZATIME);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
+        blck();
         addToBot(new ResolveNextTurnEffectsAction());
     }
 
     @Override
     public void upp() {
-        upgradeBaseCost(0);
-        //upgradeMagicNumber(1);
+        upgradeBlock(4);
         name = originalName = cardStrings.EXTENDED_DESCRIPTION[0];
         initializeTitle();
         setMonsterData(MonsterEnum.FORTIWINX);
