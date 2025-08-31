d38da9f37521139fa14190159fcad1f498e5bfc8
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-08-31 00:50:38 -0300

    MakeCardsComponent no longer scales with cost

--- src/main/java/RangerCaptain/cardfusion/components/MakeCardsComponent.java
+++ src/main/java/RangerCaptain/cardfusion/components/MakeCardsComponent.java
@@ -67,10 +67,7 @@ public class MakeCardsComponent extends AbstractComponent {
 
     @Override
     public boolean scalesWithCost() {
-        if (reference == null) {
-            return false;
-        }
-        return reference.type != AbstractCard.CardType.STATUS && reference.type != AbstractCard.CardType.CURSE;
+        return false;
     }
 
     @Override
