19a9758ee535fdee6b4591b260caab78d661a00a
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2025-11-22 13:57:57 -0400

    Intercepting should now work when played via card or triggered via power

--- src/main/java/RangerCaptain/powers/InterceptingPower.java
+++ src/main/java/RangerCaptain/powers/InterceptingPower.java
@@ -1,13 +1,16 @@
 package RangerCaptain.powers;
 
 import RangerCaptain.MainModfile;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
 public class InterceptingPower extends AbstractEasyPower {
     public static final String POWER_ID = MainModfile.makeID(InterceptingPower.class.getSimpleName());
@@ -19,6 +22,22 @@ public class InterceptingPower extends AbstractEasyPower {
 
     public InterceptingPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+        // This is also extremely jank
+        MainModfile.safeEffectQueue.add(new AbstractGameEffect() {
+            @Override
+            public void update() {
+                this.isDone = true;
+                if (AbstractDungeon.actionManager.actions.stream().noneMatch(a -> a instanceof UseCardAction)) {
+                    justEvoked = false;
+                }
+            }
+
+            @Override
+            public void render(SpriteBatch spriteBatch) {}
+
+            @Override
+            public void dispose() {}
+        });
     }
 
     @Override
@@ -33,7 +52,7 @@ public class InterceptingPower extends AbstractEasyPower {
     @Override
     public void onAfterUseCard(AbstractCard card, UseCardAction action) {
         if (justEvoked) {
-            justEvoked = false;
+            justEvoked = false; // TODO fails to work when given by power
         } else {
             primed = true;
             if (card.type != AbstractCard.CardType.POWER) {
