7c79aae21756367b07b57188f5d10e66ca52ec8a
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2025-12-08 05:29:00 -0400

    Rework LiftOff

--- src/main/java/RangerCaptain/cards/LiftOff.java
+++ src/main/java/RangerCaptain/cards/LiftOff.java
@@ -3,11 +3,9 @@ package RangerCaptain.cards;
 import RangerCaptain.cards.abstracts.AbstractEasyCard;
 import RangerCaptain.damageMods.effects.LightningOrbVFX;
 import RangerCaptain.powers.APBoostPower;
-import RangerCaptain.powers.NextTurnTakeDamagePower;
 import RangerCaptain.util.Wiz;
 import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -18,7 +16,7 @@ public class LiftOff extends AbstractEasyCard {
 
     public LiftOff() {
         super(ID, 2, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
-        baseDamage = damage = 10;
+        baseDamage = damage = 9;
         baseMagicNumber = magicNumber = 1;
         DamageModifierManager.addModifier(this, new LightningOrbVFX());
     }
@@ -26,7 +24,8 @@ public class LiftOff extends AbstractEasyCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         p.useJumpAnimation();
-        Wiz.applyToEnemy(m, new NextTurnTakeDamagePower(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+        dmg(m, AbstractGameAction.AttackEffect.NONE);
+        //Wiz.applyToEnemy(m, new NextTurnTakeDamagePower(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
         Wiz.applyToSelf(new APBoostPower(p, magicNumber));
     }
 
