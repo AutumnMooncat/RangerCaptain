c1671d3a5cf79d959b4f493193cbe70b88bba679
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2025-12-09 17:00:11 -0400

    Stash top cards now triggers reshuffle if draw pile empties

--- src/main/java/RangerCaptain/actions/StashTopCardsAction.java
+++ src/main/java/RangerCaptain/actions/StashTopCardsAction.java
@@ -3,6 +3,7 @@ package RangerCaptain.actions;
 import RangerCaptain.ui.StashedCardManager;
 import RangerCaptain.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 import java.util.ArrayList;
@@ -11,19 +12,25 @@ import java.util.function.Consumer;
 
 public class StashTopCardsAction extends AbstractGameAction {
     private final Consumer<List<AbstractCard>> callback;
+    private final List<AbstractCard> stashed;
+
     public StashTopCardsAction(int amount) {
         this(amount, l -> {});
     }
 
     public StashTopCardsAction(int amount, Consumer<List<AbstractCard>> callback) {
+        this(amount, new ArrayList<>(), callback);
+    }
+
+    private StashTopCardsAction(int amount, List<AbstractCard> processed, Consumer<List<AbstractCard>> callback) {
         this.actionType = ActionType.CARD_MANIPULATION;
         this.amount = amount;
         this.callback = callback;
+        this.stashed = processed;
     }
 
     @Override
     public void update() {
-        List<AbstractCard> stashed = new ArrayList<>();
         while (amount > 0 && !Wiz.adp().drawPile.isEmpty()) {
             AbstractCard top = Wiz.adp().drawPile.getTopCard();
             Wiz.adp().drawPile.removeCard(top);
@@ -31,7 +38,15 @@ public class StashTopCardsAction extends AbstractGameAction {
             stashed.add(top);
             amount--;
         }
+
+        if (amount > 0 && !Wiz.adp().discardPile.isEmpty()) {
+            addToTop(new StashTopCardsAction(amount, stashed, callback));
+            addToTop(new EmptyDeckShuffleAction());
+            this.isDone = true;
+            return;
+        }
+
         callback.accept(stashed);
-        this.isDone = true;
+        isDone = true;
     }
 }
